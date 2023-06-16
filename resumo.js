da6b40cc8e08bef320625e76fa3d8cb0a44590b7
Raul <raul.dev@hotmail.com>
2023-06-16 12:19:29 -0300

    resumo de operadores

---
+++ resumo.js
@@ -0,0 +1,8 @@
+//Nesta aula eu vi:
+//a diferenca entre fazer comparacoes (==), em que o JS faz a conversao entre os tipos de variaveis antes de fazer a comparacao. Ja (===) a comparacao deve ser entre o tipo e o valor da variavel
+
+//como se escreve um operador ternario usando o "?"; que acompanha uma possibilidade "true", um ":" e a possibilidade "false". Ou seja, comparacao ? true : false.
+//o porque do nome "operador ternario": deve-se ao fato de termos 3 operadores em uma unica linha. Sao eles o de comparacao (==,>=....), o operador que separa a condicao (?) e operador dois pontos (:), que separa as possibilidades (true,false).
+
+//o uso de template literal (``) que facilita a concatenecao de strings deixando a linha mais compacta e facil de ser lida. Foi introduzida no ES6.
+//Como usar o template literal escrevendo textos atraves de ascentos graves (`) e colocando variaveis com ${variavel} para que o valor deles fique nessa posicao. REVISAR juntando-conhecimento.js PARA ENTENDER MELHOR.
